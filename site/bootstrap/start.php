41a5be9c3def85beaabc5282079772488b55d7f7
Devaddiction <fjalvarez@gmail.com>
2013-10-06 20:15:43 +0200

    Environmnent

--- site/bootstrap/start.php
+++ site/bootstrap/start.php
@@ -28,7 +28,9 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+    'local' => array('*.dev', 'localhost'),
+    'stage' => array('test.devaddiction.com'),
+    'production' => array('devaddiction.com')
 
 ));
 
